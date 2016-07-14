2c76361b015bf1dd8613c85461d734d33c615d51
cetrobo <achraf.benaomar@interactive-object.com>
2016-07-14 01:45:52 +0100

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,20 @@
-var gulp 		 = require('gulp');
-var sass 		 = require('gulp-sass');
-var less 		 = require('gulp-less');
-var concat       = require('gulp-concat');
-var minifyCss    = require('gulp-minify-css');
-var autoprefixer = require('gulp-autoprefixer');
-var sourceMaps   = require('gulp-sourcemaps');
-var rename       = require('gulp-rename');
-var browserSync  = require('browser-sync').create();
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var less        = require('gulp-less');
+var browserSync = require('browser-sync').create();
 
-
-gulp.task('serve',['build-css'], function() {
+gulp.task('serve',['sass'], function() {
     browserSync.init({
         server: {
             baseDir: "./web"
         }
     });
-    gulp.watch(["web/sass/abstracts/*.scss",
-        "web/sass/base/*.scss",
-        "web/sass/pages/*.scss",
-        "web/sass/layout/*.scss",
-        "web/sass/components/*.scss"], ['build-css']);
+    gulp.watch("web/sass/*.scss", ['sass']);
     gulp.watch("web/*.html").on('change', browserSync.reload);
 });
-
-
-gulp.task('build-css', function() {
-  return gulp.src(["web/sass/pages/*.scss","web/sass/layout/*.scss",
-    "web/sass/components/*.scss",
-    "web/sass/base/*.scss",
-    "web/sass/abstracts/*.scss"
-    ])
+gulp.task('sass', function() {
+  return gulp.src('web/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(concat('main.css'))
-    .pipe(autoprefixer())
-    .pipe( minifyCss( {
-                compatibility: 'ie8'
-      } ) )
-    .pipe( rename( {
-                suffix: '.min'
-     } ) )
-    .pipe( sourceMaps.write() )
     .pipe(gulp.dest('web/css'))
     .pipe(browserSync.stream());
 });
